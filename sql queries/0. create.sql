b796ea2447dfda4f462952fcbcc5da8968f2a627
MB <matbochenek42@gmail.com>
2025-11-16 22:05:13 +0100

    Update file paths in SQL create script

--- sql queries/0. create.sql
+++ sql queries/0. create.sql
@@ -13,7 +13,7 @@ CREATE TABLE former_names --zmiana nazw państw
 
 ALTER TABLE public.former_names OWNER to postgres;
 
-COPY former_names FROM 'C:\Users\Mateusz\Desktop\ZBIORCZE\TECH\ANALIZA DANYCH\SQL\VS\Projekty MB\International football results from 1872 to 2025\data\former_names.csv' DELIMITER ',' CSV HEADER;
+COPY former_names FROM '...\International football results from 1872 to 2025\data\former_names.csv' DELIMITER ',' CSV HEADER;
 
 
 
@@ -31,7 +31,7 @@ CREATE TABLE goalscorers --dane zawierające informacje o strzelcach
 
 ALTER TABLE public.goalscorers OWNER to postgres;
 
-COPY goalscorers FROM 'C:\Users\Mateusz\Desktop\ZBIORCZE\TECH\ANALIZA DANYCH\SQL\VS\Projekty MB\International football results from 1872 to 2025\data\goalscorers.csv' DELIMITER ',' CSV HEADER NULL 'NA';
+COPY goalscorers FROM '...\International football results from 1872 to 2025\data\goalscorers.csv' DELIMITER ',' CSV HEADER NULL 'NA';
 
 
 CREATE TABLE results -- wyniki meczów
@@ -49,7 +49,7 @@ CREATE TABLE results -- wyniki meczów
 
 ALTER TABLE public.results OWNER to postgres;
 
-COPY results FROM 'C:\Users\Mateusz\Desktop\ZBIORCZE\TECH\ANALIZA DANYCH\SQL\VS\Projekty MB\International football results from 1872 to 2025\data\results.csv' DELIMITER ',' CSV HEADER;
+COPY results FROM '...\International football results from 1872 to 2025\data\results.csv' DELIMITER ',' CSV HEADER;
 
 
 CREATE TABLE shootouts -- karne 
@@ -63,7 +63,7 @@ CREATE TABLE shootouts -- karne
 
 ALTER TABLE public.shootouts OWNER to postgres;
 
-COPY shootouts FROM 'C:\Users\Mateusz\Desktop\ZBIORCZE\TECH\ANALIZA DANYCH\SQL\VS\Projekty MB\International football results from 1872 to 2025\data\shootouts.csv' DELIMITER ',' CSV HEADER;
+COPY shootouts FROM '...\International football results from 1872 to 2025\data\shootouts.csv' DELIMITER ',' CSV HEADER;
 
 
 
